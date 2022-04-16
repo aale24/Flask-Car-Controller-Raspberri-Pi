bd2b40dcd5be99d4a85b90c253e68df4fc0bff1d
aale24 <alejandroperezpestana99@gmail.com>
2022-04-16 17:55:18 -0400

    Web Formatting Done
    HTML and CSS done

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ button.onclick = function() {
         div.style.display = 'block';
     }
 };
+
+function resizeToMax(id){
+    myImage = new Image() 
+    var img = document.getElementById(id);
+    myImage.src = img.src; 
+    if(myImage.width / document.body.clientWidth > myImage.height / document.body.clientHeight){
+        img.style.width = "100%";
+    } else {
+        img.style.height = "100%";
+    }
+}
+
